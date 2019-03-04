cf16135248202b57cfd7bfa2bcf297f3d53591c5
joel <hedwigg01@gmail.com>
2019-03-03 19:44:43 -0700

    Added dictionary option for passed in args.
    Still need to fix filereader to work with all text files and possibly speed up the process (threading?).

--- 3assignment/assignment3.java
+++ 3assignment/assignment3.java
@@ -1,12 +1,10 @@
 /*
 NOTES:
 2 of the 6 are all digits  breakable with dictionary : digits 1-99999999
-2 (QINGFANG & wakemeupwhenseptemberends) are breakable with crackstation-human-only.txt
+(QINGFANG & wakemeupwhenseptemberends & 181003) are breakable with crackstation-human-only.txt
+victorboy and lion8888 QINGFANG wakemeupwhenseptemberends
 
 */ 
-
-
-
 
 import java.io.BufferedReader;
 import java.math.BigInteger; 
@@ -26,10 +24,12 @@ public class assignment3 {
     public static ArrayList<String> hashesToDecrypt = new ArrayList<String>();
     public static ArrayList<String> dictionaryFiles = new ArrayList<String>();
     public static int dictionaryCount;
-
+    public static double startTime; //program start time
+    public static double endTime; 
 
 	public static void main(String args[]) throws NoSuchAlgorithmException 
 	{ 
+        startTime = System.currentTimeMillis();
         hashesToDecrypt.add("6f047ccaa1ed3e8e05cde1c7ebc7d958");
         hashesToDecrypt.add("275a5602cd91a468a0e10c226a03a39c");
         hashesToDecrypt.add("b4ba93170358df216e8648734ac2d539");
@@ -37,16 +37,29 @@ public class assignment3 {
         hashesToDecrypt.add("8cd9f1b962128bd3d3ede2f5f101f4fc");
         hashesToDecrypt.add("554532464e066aba23aee72b95f18ba2");
 
+        //dictionaryFile.add(args[0]);
+
         //dictionaryFiles.add("digits1-99999999.txt");
         //dictionaryFiles.add("crackstation-human-only.txt");
         dictionaryFiles.add("crackstation.txt");
-
+        //dictionaryFiles.add("probable-top12000.txt");
+        //dictionaryFiles.add("darkweb2017-top10000.txt"); //not working - need to change filereader
+        
 
         dictionaryCount = 0;
+        System.out.println(dictionaryFiles.size() + " # txt files loaded");
         while(hashesToDecrypt.size() > 0)
         {
-            System.out.println("--- Using dictionary " + dictionaryFiles.get(dictionaryCount));
-            useDictionary(dictionaryFiles.get(dictionaryCount));
+            try
+            {
+                System.out.println("--- Using dictionary " + dictionaryFiles.get(dictionaryCount));
+                useDictionary(dictionaryFiles.get(dictionaryCount));
+            }catch(IndexOutOfBoundsException e)
+            {
+                //exit loop if there are no more dictionary files to use.
+                break;
+            }
+            
         }
         System.out.println("\n*** Program done ***\n");
 
@@ -62,8 +75,7 @@ public class assignment3 {
             String hashOfDictionaryEntry;
             while(true)
             {   
-                dictionaryEntry = fileReader.readLine();
-
+                dictionaryEntry = fileReader.readLine(); //***** Doesnt read some files
                 //if we have reached the end of the dictionary, end program.
                 if(dictionaryEntry == null)
                 {
@@ -79,8 +91,14 @@ public class assignment3 {
                 //compare generated hash to arraylist of given hashes we are meant to decrypt.
                 if(hashesToDecrypt.contains(hashOfDictionaryEntry))
                 {
+                    //grab decrypt time (in ms) for the current hash
+                    endTime = System.currentTimeMillis() - startTime;
+                    //convert to seconds
+                    endTime = endTime/Long.valueOf(1000);
+
                     //if a match, print result
-                    System.out.println("The password for hash value " + hashOfDictionaryEntry + " is " + dictionaryEntry + ", it takes the program <SECONDS> to recover this password.");
+                    handleResult(hashOfDictionaryEntry, dictionaryEntry, endTime);
+                    System.out.println("The password for hash value " + hashOfDictionaryEntry + " is " + dictionaryEntry + ", it takes the program " + endTime +" seconds to recover this password with the " + dictionary + " dictionary");
                     //remove hash from hashesToDecrypt
                     hashesToDecrypt.remove(hashOfDictionaryEntry);
                 }
